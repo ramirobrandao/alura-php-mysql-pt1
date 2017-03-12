193b5d886b052a0284cc3a4d28cb5c11f8ae9d61
ramirobrandao <ramirobrandao@outlook.com>
2017-03-12 19:20:38 -0300

    alteração no arquivo

--- sobre.php
+++ sobre.php
@@ -1,5 +1,5 @@
 <?php include("cabecalho.php"); ?>
 
-<h1><p class="alert alert-info">Sistema Web em Produção.</p></h1>
+<h1><p class="alert-info">Sistema Web em Produção.</p></h1>
 
 <?php include("rodape.php"); ?>
